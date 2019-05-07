c5f2efb1249e9abdc317d56ad6b51d97f7c2d161
cclivick <46388110+cclivick@users.noreply.github.com>
2019-05-07 18:25:53 -0500

    Second Commit - First for loop complete and buttons styled

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+        
+//Create array of reaction buttons    
+        var buttonArray = [
+            "shocked",
+            "angry",
+            "happy",
+            "sad",
+            "bored",
+            "miserable",
+            "confused",
+            "excited",
+            "irritated",
+            "annoyed",
+            "intrigued",
+            "elated",
+            "bashful",
+            "fatigued",
+            "gloomy",
+            "jealous",
+            "lonely",
+            "naughty",
+            "panicked",
+            "serene",
+            "victorious",
+            "violent",
+            "zany"
+        ]
+
+//For loop to create buttons from array items
+        for(var i = 0; i < buttonArray.length; i++) {
+            var buttons = $('<button id=reactionBtn>' + buttonArray[i] + '</button>');
+            buttons.appendTo($('#buttonRow'));
+        }
+//Create function which takes user input from form and adds text to buttonArray array
+
+
+});
