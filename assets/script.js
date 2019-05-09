162992785873f2be1f4ad7ba379d94cfeeb5db30
cclivick <46388110+cclivick@users.noreply.github.com>
2019-05-09 14:50:25 -0500

    Gifs generating from existing buttons - dynamically created buttons not working

--- assets/script.js
+++ assets/script.js
@@ -40,10 +40,33 @@ $(document).ready(function() {
             newButton.appendTo($('#buttonRow'));
         })
 //Create function which generates gifs when user clicks reactionBtns
+        //Create url for giphy API on button click
         $("button").click(function() {
             console.log("clicked");
             var buttonText = $(this).attr('id');
-            console.log(buttonText);
+            var apikey = "vZnidhqhpNZPO651y0Y99NnMGh197Sz4";
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+              buttonText + "&api_key=" + apikey + "&limit=10";
+        //Get array of objects from giphy API
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+              }).then(function(response) {
+                console.log(response);
+                //Add gifs to page
+                var results = (response.data);
+
+                for(var j = 0; j < results.length; j++) {
+                    var reactionGifDiv = $("<div>");
+                    var rated = $("<p>");
+                    $("p").text("Rated: " + results[j].rating);
+                    var reactionGif = $("<img src=" + results[j].images.fixed_height.url + ">");
+                    rated.appendTo(reactionGifDiv);
+                    reactionGif.appendTo(reactionGifDiv);
+                    reactionGifDiv.prependTo($("#gifDiv"));
+                }
+
         })
 
+})
 });
